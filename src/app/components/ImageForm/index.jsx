3bc231a443b237265a8605e07dab641e4bb6bf1f
Siddhesh Sutar <siddheshsutar96@gmail.com>
2023-09-23 22:09:14 +0530

    render img WIP

--- src/app/components/ImageForm/index.jsx
+++ src/app/components/ImageForm/index.jsx
@@ -14,28 +14,96 @@ const ImageForm = () => {
     const [previewImage, setPreviewImage] = useState('');
     const [previewTitle, setPreviewTitle] = useState('');
 
+    const listRef = ref(storage, 'images');
+    
+    const fetchImages = async () => {
+        const resp = await listAll(listRef)
+            // .then((res) => {
+            //     res.prefixes.forEach((folderRef) => {
+            //         // All the prefixes under listRef.
+            //         // You may call listAll() recursively on them.
+            //     });
+            //     res.items.forEach((itemRef) => {
+            //         // All the items under listRef.
+
+            //     });
+            //     return resp
+            // }).catch((error) => {
+            //     // Uh-oh, an error occurred!
+            // });
+
+            // return resp
+
+        // let result = await storageRef.child('images').listAll();
+        // let result = await ref(storage, 'images').listAll();
+        // let urlPromises = result.items.map(imageRef => imageRef.getDownloadURL());
+
+        // return Promise.all(urlPromises);
+
+        const storageRef = ref(storage, 'images');
+    const result = await listAll(storageRef);
+  
+    const urlPromises = result.items.map((imageRef) => getDownloadURL(imageRef));
+  
+    return Promise.all(urlPromises);
+
+    }
+
     useEffect(() => {
         // const listRef = ref(storage, 'images');
         const imRef = ref(storage, 'images/polling_ER_1.5.2.PNG');
+
+        // const loadImages = async () => {
+        //     const urls = await fetchImages();
+        //     console.log('hex: ', urls)
+        //     const img = document.getElementById('preview-img');
+        //     img.setAttribute('src', urls[0]);
+        //     // setFiles(urls);
+
+
+        // }
+        // loadImages();
+        
 
         getDownloadURL(ref(storage, 'images/polling_ER_1.5.2.PNG'))
             .then((url) => {
 
                 // This can be downloaded directly:
-                const xhr = new XMLHttpRequest();
-                xhr.responseType = 'blob';
-                xhr.onload = (event) => {
-                    const blob = xhr.response;
-                };
-                xhr.open('GET', url);
-                xhr.send();
+                // const xhr = new XMLHttpRequest();
+                // xhr.responseType = 'blob';
+                // xhr.onload = (event) => {
+                //     const blob = xhr.response;
+
+                // };
+                // xhr.open('GET', url);
+                // xhr.send();
 
                 // Or inserted into an <img> element
+            console.log('hexD: ', url)
+
                 const img = document.getElementById('preview-img');
                 img.setAttribute('src', url);
             })
             .catch((error) => {
-                // Handle any errors
+                // A full list of error codes is available at
+                // https://firebase.google.com/docs/storage/web/handle-errors
+                switch (error.code) {
+                    case 'storage/object-not-found':
+                        // File doesn't exist
+                        break;
+                    case 'storage/unauthorized':
+                        // User doesn't have permission to access the object
+                        break;
+                    case 'storage/canceled':
+                        // User canceled the upload
+                        break;
+
+                    // ...
+
+                    case 'storage/unknown':
+                        // Unknown error occurred, inspect the server response
+                        break;
+                }
             });
 
     }, [])
@@ -96,7 +164,8 @@ const ImageForm = () => {
                     <Button icon={<UploadOutlined />}>Click to Upload</Button>
                 </Upload>
                 {
-                    previewOpen && <img id="preview-img" alt="img" style={{ width: '100%' }} src={previewImage} />
+                    // previewOpen && <img id="preview-img" alt="img" style={{ width: '100%' }} src={previewImage} />
+                    true && <img id="preview-img" alt="img" width={100} height={100} src={previewImage} />
                 }
             </div>
         </div>
