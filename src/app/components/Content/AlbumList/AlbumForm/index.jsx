de74d2b29221272eb9bcc470dc0c78102569d20b
Siddhesh Sutar <siddheshsutar96@gmail.com>
2023-09-24 18:16:12 +0530

    albumform: validation

--- src/app/components/Content/AlbumList/AlbumForm/index.jsx
+++ src/app/components/Content/AlbumList/AlbumForm/index.jsx
@@ -3,20 +3,27 @@ import styles from './index.module.scss'
 import { addDoc, collection } from 'firebase/firestore'
 import { db } from '../../../../../../fireStore'
 import { ALBUM_DB_NAME } from '../../../../../../constants'
+import CustomSnackbar from '@/app/components/Snackbar'
 
 const AlbumForm = () => {
 
     const [albumName, setAlbumName] = useState('')
+    const [open, setopen] = useState(false)
     const inp = useRef(null)
 
     const handleSubmit = async (e) => {
         e.preventDefault()
 
+        if (!albumName) {
+            setopen(true)
+            inp.current.focus()
+            return
+        }
+
         const albumRef = collection(db, ALBUM_DB_NAME);
         const docRef = await addDoc(albumRef, {
             name: albumName
         });
-        console.log('hex: ', docRef.id)
 
         inp.current.focus()
     }
@@ -36,6 +43,12 @@ const AlbumForm = () => {
                 >Clear</button>
                 <button type='submit'>Create</button>
             </form>
+            <CustomSnackbar
+                open={open}
+                onClose={() => setopen(false)}
+                message="Please enter album name"
+                severity={"error"}
+            />
         </div>
     );
 }
---
+++ src/app/components/Snackbar/index.jsx
@@ -0,0 +1,23 @@
+import { Alert, Snackbar } from "@mui/material";
+
+const CustomSnackbar = ({
+    open,
+    onClose,
+    severity,
+    message
+}) => {
+    return (
+            <Snackbar
+                open={open}
+                autoHideDuration={3000}
+                anchorOrigin={{ vertical: 'top', horizontal: 'center' }}
+                onClose={onClose}
+            >
+                <Alert onClose={onClose} severity="error" sx={{ width: '100%' }}>
+                    {message}
+                </Alert>
+            </Snackbar>
+    );
+}
+ 
+export default CustomSnackbar;
